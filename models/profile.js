1812f2e6e58c7d92116ab0b652e982674a2f0bba
besv19ab <besv19ab@student.cbs.dk>
2020-04-23 00:05:52 +0200

    start på mongodb, Betina

---
+++ models/profile.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const profileSchema = new Schema({
+    title: String,
+    body: String
+});
+
+const profile = mongoose.model('profile', profileSchema);
+
+module.exports = profile
+
