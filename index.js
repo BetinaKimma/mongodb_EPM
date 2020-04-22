1ba5c88017ec2a0cc216a92858d4189e93dcfa2d
besv19ab <besv19ab@student.cbs.dk>
2020-04-22 17:18:35 +0200

    first commit Betina

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-
+const express = require('express');
 const http = require('http');
 const fs = require('fs');
-//const HomePage = fs.readFileSync('index.html');
-//const signInPage = fs.readFileSync('sigIn.html');
+const homePage = fs.readFileSync('index.html');
+const signInPage = fs.readFileSync('signIn.html');
 //const userProfilePage = fs.readFileSync('userProfile.html');
-//const notFoundPage = fs.readFileSync('notFound.html');
+const notFoundPage = fs.readFileSync('notFound.html');
 
-const express = require('express');
+
 const app = express();
-
 app.listen(3000, () =>{
     console.log("App listening on port 3000");
 });
 
 
+const server = http.createServer((req, res) => {
+    if(req.url === '/')
+        res.end(homePage)
+    else if(req.url === '/signIn')
+        res.end(signInPage)
+    else {
+        res.writeHead(404)
+        res.end(notFoundPage)
+    }
+    console.log(req.url)
+    res.end('Hello Node.js')
+})
 
-//const server = http.createServer((req, res) =>{
-//    if(req.url === '/userProfile')
-//        res.end(userProfilePage)
-//    else if(req.url === '/signIn')
-//        res.end(signInPage)
-//    else {
-//        res.writeHead(404)
-//        res.end(notFoundPage)
-//    }
-//    console.log(req.url);
-//    res.end('Hello Node.js');
-//});
-//server.listen(3000)
+server.listen(3000)
 
 
