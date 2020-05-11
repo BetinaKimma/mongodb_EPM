acfc4266a2635a21146e9f4826aed6e841e1cedf
besv19ab <besv19ab@student.cbs.dk>
2020-05-11 18:02:53 +0200

    dato på forsiden, fordi vi allerede havde koden Betina

--- public/js/userFunctions.js
+++ public/js/userFunctions.js
@@ -1,3 +1,19 @@
+// BKS. Dette er funktionen der henter dato og tid,
+// kode taget fra W3Schools: https://www.w3schools.com/js/tryit.asp?filename=tryjs_date_string_iso1 og
+// StackOverflow: https://stackoverflow.com/questions/10211145/getting-current-date-and-time-in-javascript
+// fundet den 09/02-20. Det er en færdig implementeret funktion.
+var currentdate = new Date();
+var d = currentdate.getDate() + "/"
+    + (currentdate.getMonth()+1)  + "/"
+    + currentdate.getFullYear() + " @ "
+    + currentdate.getHours() + ":"
+    + currentdate.getMinutes() + ":"
+    + currentdate.getSeconds();
+if (document.getElementById("dateOnFront") != null)
+{
+    document.getElementById("dateOnFront").innerHTML = d;
+}
+
 // dette er en færdig implementeret funktion der gemmer profiltekst i local storage
 // bliver ikke længere kaldt, da vi gemmer i database i stedet for
 function saveText() {
